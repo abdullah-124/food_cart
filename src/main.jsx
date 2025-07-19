33fa175d145848830bac022e4dc169fdee9494c3
abdullah-124 <mdabdullahsakib468@gmail.com>
2025-07-19 20:14:07 +0600

    deploy

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/food_cart">
       <App />
     </BrowserRouter>
   </StrictMode>
